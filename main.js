d1de2c74817baa4836441ed6abb6494eff7bb052
BachAmadou <bafosth@yahoo.fr>
2019-10-22 01:02:52 -0400

    updated the script for the second search bar

--- main.js
+++ main.js
@@ -2,5 +2,30 @@
 
 function displayText() {
     const inputText = document.querySelector(".textTyping").value;
-    document.querySelector(".cardParagraph").innerHTML = inputText;
+    const cardParagraphs = document.querySelectorAll(".cardParagraph");
+
+    for (let i = 0; i < cardParagraphs.length; i++) {
+        cardParagraphs[i].innerHTML = inputText;
+    }
+}
+
+
+// Get the font size updated when the user selects a size option
+// function ChangeFont(select) {
+//     // Return the index of the selected option
+//     let sizeIndex = select.selectedIndex;
+
+//     // Return selected size values
+//     let sizeOption = select.options[sizeIndex].text;
+//     let fontSize = document.getElementById('fontText');
+
+//     if ('fontSizeAdjust' in fontText.style) {
+//         fontText.style.fontSizeAdjust = sizeOption;
+//     }
+
+// }
+
+function ChangeFont(selectFont) {
+    let sizeIndex = selectFont.options[selectFont.selectedIndex].index;
+    document.getElementById('fontText').style.fontSize = sizeIndex;
 }
